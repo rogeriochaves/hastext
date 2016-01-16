a7b99e2f52662123c5f18b6141179a092336f1b7
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-16 16:10:32 -0200

    Add gulp to watch files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    browserSync = require('browser-sync').create()
+
+gulp.task('build-tests', shell.task([
+  'elm-make test/TestRunner.elm --output dist/test/index.html --warn'
+]))
+
+gulp.task('run-tests', ['build-tests'], function () {
+  browserSync.reload()
+})
+
+gulp.task('default', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/test"
+    }
+  });
+  gulp.run('run-tests');
+
+  gulp.watch('src/**/*.elm', ['run-tests'])
+  gulp.watch('test/**/*.elm', ['run-tests'])
+});
